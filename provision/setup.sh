58072febc560b57d9ef16e3401d90b0ae53df3b2
skeeso <eric@arpa.ph>
2023-06-21 09:31:02 +0800

    Update setup.sh

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 apt-get update
 apt-get -y install aptitude dnsmasq python python-pexpect software-properties-common --no-install-recommends
 apt-add-repository --yes --update ppa:ansible/ansible
